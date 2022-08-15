5b2f5cca22115f3beb2caaa1b6c9f83ec30d1e47
Guilherme José <101995065+GuilhermeJQA@users.noreply.github.com>
2022-08-15 20:01:22 -0300

    Criado usando o Colaboratory

---
+++ Notas.py
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Notas.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyOHqVdM4W/36C5bwcsuxctp"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "4c4MCmQin1aE"
+      },
+      "outputs": [],
+      "source": [
+        "nota1 = int(input(\"Digite a nota 1: \"))\n",
+        "nota2 = int(input(\"Digite a nota 2: \"))\n",
+        "nota3 = int(input(\"Digite a nota 3: \"))\n",
+        "media = (nota1 + nota2 + nota3)/3\n",
+        "print(media)\n",
+        "if media >= 7 and media <10:\n",
+        "  print(\"Parábens você passou\")\n",
+        "elif media == 10:\n",
+        "  print(\"Aprovado com distinção\")\n",
+        "else:\n",
+        "  print(\"Você reprovou\")\n",
+        "    \n"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        ""
+      ],
+      "metadata": {
+        "id": "KqYQm9QxrQ72"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
